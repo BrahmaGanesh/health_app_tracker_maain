7b5593643de83b06eaba7cdc2d633c9bdb20aea0
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2026-07-09 22:17:14 +0530

    file

--- utils/gemini_ai.py
+++ utils/gemini_ai.py
@@ -7,7 +7,7 @@ print("Gemini Key Loaded:", bool(api_key))
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 def ask_gemini(message, history=None):
