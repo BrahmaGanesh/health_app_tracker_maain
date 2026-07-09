1549594f84d8f07fc718a9222bb5de83ce459752
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2026-07-09 21:46:16 +0530

    file

--- routes/api/ai_assistant_api.py
+++ routes/api/ai_assistant_api.py
@@ -1,22 +1,20 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
-
 from utils.gemini_ai import ask_gemini
 
 ai_bp = Blueprint("ai", __name__)
 
-
 @ai_bp.route("/chat", methods=["POST"])
 @jwt_required()
 def chat():
+    print("===== AI ROUTE CALLED =====")
 
-    data = request.get_json()
-
+    data = request.get_json() or {}
     message = data.get("message", "")
 
-    history = data.get("history", [])
+    result = ask_gemini(message)
 
-    result = ask_gemini(message, history)
+    print(result)
 
     return jsonify({
         "success": result["success"],
